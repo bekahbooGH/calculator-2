cf04a622f9d38d931883d34acfcae0f9cbc5b699
vagrant <vagrant@vagrant.vm>
2020-10-01 00:16:11 +0000

    Finished calculator while loop with function calls.

--- calculator.py
+++ calculator.py
@@ -1,7 +1,47 @@
 """CLI application for a prefix-notation calculator."""
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
+                        power, mod)
 
 
-# Replace this with your code
+while True:
+    exp = input("Enter an expression: ")
+    exp_tokens = exp.split(' ')
+    if exp_tokens[0] == "q" or exp_tokens[0] == "Q":
+        break
+    else:
+        for index in range(1, len(exp_tokens)):
+            exp_tokens[index] = int(exp_tokens[index])
+        # print(exp_tokens)
+        # print(isinstance(exp_tokens[1], int))
+        # print(type(exp_tokens[1]))
+        if exp_tokens[0] == "+":
+            answer = (add(exp_tokens[1], exp_tokens[2]))
+
+        elif exp_tokens[0] == "-":
+            answer = subtract(exp_tokens[1], exp_tokens[2])
+
+        elif exp_tokens[0] == "*":
+            answer = multiply(exp_tokens[1], exp_tokens[2])
+        
+        elif exp_tokens[0] == "/":
+            answer = divide(exp_tokens[1], exp_tokens[2])
+
+        elif exp_tokens[0] == "square":
+            answer = square(exp_tokens[1])
+
+        elif exp_tokens[0] == "cube":
+            answer = cube(exp_tokens[1])
+
+        elif exp_tokens[0] == "pow":
+            answer = power(exp_tokens[1], exp_tokens[2])
+
+        elif exp_tokens[0] == "mod":
+            answer = mod(exp_tokens[1], exp_tokens[2])
+
+        else:
+            answer = 'Invalid expression.'
+
+    print(answer)
+
+
